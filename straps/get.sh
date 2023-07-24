913b31a8124564ce2066d893d720aa9190e4e16b
Kurt Kremitzki <kurt@kwk.systems>
2023-07-24 13:27:18 -0500

    Switch from hardcoded dry-run in  script

--- straps/get.sh
+++ straps/get.sh
@@ -2,8 +2,9 @@
 ISO_URL=https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-12.1.0-amd64-netinst.iso
 CHECKSUM_URL=https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/SHA512SUMS
 SIGNATURE_URL=https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/SHA512SUMS.sign
-CMD="echo"
-#CMD="wget"
+# Replace switching this with --dry-run flag support
+#CMD="echo"
+CMD="wget"
 
 ${CMD} "${ISO_URL}"
 ${CMD} "${CHECKSUM_URL}"
