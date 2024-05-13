a6487fea50f1696d3ed7d3f0643ee690bdb23467
Kurt Kremitzki <kurt@kwk.systems>
2024-05-13 15:27:27 -0500

    Add note about upstreaming

--- straps/verify.sh
+++ straps/verify.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+# Should perhaps be documented here, but isn't:
+# https://www.debian.org/CD/verify
 gpg --no-default-keyring --keyring /usr/share/keyrings/debian-role-keys.gpg --verify SHA512SUMS.sign
